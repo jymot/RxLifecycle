c8dcb512c119b9433561019d78351e6427f7e258
wangchao <magician.of.technique@gmail.com>
2017-11-29 13:27:11 +0800

    [*] sample fragment

---
+++ app/src/main/java/im/wangchao/rxlifecycleapp/TestFragment.java
@@ -0,0 +1,13 @@
+package im.wangchao.rxlifecycleapp;
+
+import android.app.Fragment;
+
+/**
+ * <p>Description  : TestFragment.</p>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 2017/11/29.</p>
+ * <p>Time         : 下午1:17.</p>
+ */
+public class TestFragment extends Fragment {
+
+}
---
+++ app/src/main/java/im/wangchao/rxlifecycleapp/TestV4Fragment.java
@@ -0,0 +1,12 @@
+package im.wangchao.rxlifecycleapp;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * <p>Description  : TestV4Fragment.</p>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 2017/11/29.</p>
+ * <p>Time         : 下午1:17.</p>
+ */
+public class TestV4Fragment extends Fragment {
+}
